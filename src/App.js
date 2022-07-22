4fb7fda6079fdec1d823ff7e1a1c6e28db7156e7
Sydney McGee <mcgee.sydney@gmail.com>
2022-07-22 09:22:17 -0500

    Deleted unneccessary files + added Components folder + project files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+
+
 
 function App() {
+
+  const [posts, setPosts] = useState([{name: "Sydney McGee", post: "Testing"}])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>SocialFeed</h3>
     </div>
   );
 }
